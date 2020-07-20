31cbd94203a9fd21ab9c560b2ede07d8fd2d603d
mahmud009 <mahmud.est@gmail.com>
2020-07-20 14:23:26 +0600

    Doctor selection on filter

--- functions.js
+++ functions.js
@@ -2,126 +2,122 @@
 let DOCTORS = [...DOCTORS_INFO];
 let PATIENTS = [...PATIENTS_INFO];
 
-
 // Initialization function
 //=================================
-function init(){
-
-
-
-
-}
-
+function init() {}
 
 //Function to add buttons for filtering doctors
 // Button will be created dynamically based on
 // Doctors data
 //===================================================
 
+function dynamicDoctorButtons() {
+  // Getting the unique locations
+  let all_locations = DOCTORS.map((m) => m.shortLocation.toLocaleLowerCase());
+  let locations = all_locations.filter(
+    (m, i) => all_locations.indexOf(m) === i
+  );
 
-function dynamicDoctorButtons(){
-
-    // Getting the unique locations
-    let all_locations = DOCTORS.map(m => m.shortLocation.toLocaleLowerCase());
-    let locations = all_locations.filter( (m, i) => all_locations.indexOf(m) === i);
-
-
-    // Adding buttons to the dom
-    locations.map(m => {
-       let button = `<a class="filter-btn" href="#" data-filter='${m}'>${m}</a>`;
-       $('#doctor-grid-buttons').append(button);
-    })
-    
+  // Adding buttons to the dom
+  locations.map((m) => {
+    let button = `<a class="filter-btn" href="#" data-filter='${m}'>${m}</a>`;
+    $("#doctor-grid-buttons").append(button);
+  });
 }
-
 
 //Function to add doctors image to the doctors grid
 //===================================================
-function dynamicDoctorImages(){
+function dynamicDoctorImages() {
+  // Getting all doctors info
+  let doctors = DOCTORS;
 
-    // Getting all doctors info
-    let doctors = DOCTORS;
-
-
-    // Adding images to the with a wrapper
-    // and necessary data for isotope filter
-    doctors.map((m, i) => {
-        let gridItem = `<div class='grid-item ${m.shortLocation} ${ i == 10 ? "active" : ""}' ><div class="image-bg"><img data-id="${m.id}" src="${m.imagePath}"/></div></div>`;
-        $('#doctors-grid').append(gridItem);
-    })
+  // Adding images to the with a wrapper
+  // and necessary data for isotope filter
+  doctors.map((m, i) => {
+    let gridItem = `<div class='grid-item ${m.shortLocation} ${
+      i == 10 ? "active" : ""
+    }' ><div class="image-bg"><img data-id="${m.id}" src="${
+      m.imagePath
+    }"/></div></div>`;
+    $("#doctors-grid").append(gridItem);
+  });
 }
 
 // Function to init doctros isotope
 //=====================================
 
-function doctorsIsotope(){
+function doctorsIsotope() {
+  $("#doctors-grid").isotope({
+    itemSelector: ".grid-item",
+    layoutMode: "masonry",
+    percentPosition: true,
+  });
 
-    $('#doctors-grid').isotope({
-        itemSelector: '.grid-item',
-        layoutMode : 'masonry',
-        percentPosition: true,
-      })
+  $(".filter-btn").on("click", function (e) {
+    e.preventDefault();
 
-      $('.filter-btn').on('click', function(e){
+    $(".filter-btn").removeClass("active");
+    $(this).addClass("active");
+    let filterName = $(this).data().filter;
 
-        e.preventDefault();
-
-        $('.filter-btn').removeClass('active');
-        $(this).addClass('active');
-        let filterName = $(this).data().filter;
-
-        if(filterName != '*'){
-            $('#doctors-grid').isotope({
-                filter : "."+filterName,
-            })
-        } else {
-            $('#doctors-grid').isotope({
-                filter : filterName,
-            })
-        }
-
-      })
-
+    if (filterName != "*") {
+      $("#doctors-grid").isotope({
+        filter: "." + filterName,
+      });
+    } else {
+      $("#doctors-grid").isotope({
+        filter: filterName,
+      });
+    }
+  });
 }
 
-// Function to change doctors data on 
+// Function to change doctors data on
 // grid image click
 //=====================================
 
-function doctorInfoChange(){
+function doctorInfoChange() {
+  $("#doctors-grid .grid-item img").on("click", function () {
+    $("#doctors-grid .grid-item").removeClass("active");
+    $(this).parents(".grid-item").addClass("active");
 
-    $('#doctors-grid .grid-item img').on('click', function(){
+    let doctors = DOCTORS;
+    let doctorId = $(this).data().id;
+    let doctorInfo = doctors.filter((m) => m.id == doctorId)[0];
 
-        $('#doctors-grid .grid-item').removeClass('active');
-        $(this).parents('.grid-item').addClass('active');
-
-        let doctors = DOCTORS;
-        let doctorId = $(this).data().id;
-        let doctorInfo = doctors.filter(m => m.id == doctorId)[0];
-
-
-        $("#doctor-name").text(doctorInfo.name);
-        $("#doctor-title").text(doctorInfo.title);
-        $("#doctor-awards").text(doctorInfo.achievements);
-        $("#doctor-location").text(doctorInfo.location);
-
-
-    })
-
+    $("#doctor-name").text(doctorInfo.name);
+    $("#doctor-title").text(doctorInfo.title);
+    $("#doctor-awards").text(doctorInfo.achievements);
+    $("#doctor-location").text(doctorInfo.location);
+  });
 }
 
-// Function to get all patient slider 
+// Function to change selection on doctor filter
+// based on selected filter
+//======================================================
+
+function changeDoctorSelection() {
+  $("#doctor-grid-buttons .filter-btn").on("click", function (e) {
+    e.preventDefault();
+    let doctors = DOCTORS;
+    let location = $(this).data().filter;
+    let firstId;
+    if (location == "*") {
+      firstId = "D11";
+    } else {
+      firstId = doctors.filter((m) => m.shortLocation === location)[0].id;
+    }
+    $(`#doctors-grid img[data-id=${firstId}]`).click();
+  });
+}
+
+// Function to get all patient slider
 // Based on patient data
 
-function setPatientData(){
+function setPatientData() {
+  let patients = PATIENTS;
 
-    let patients = PATIENTS;
-
-
-    patients.map((m, i) => {
-
-    
-
+  patients.map((m, i) => {
     let singleItem = `
     
     <div class="patient-info-single" data-id="${i}">
@@ -216,74 +212,62 @@ function setPatientData(){
 
     `;
 
-
-    $('#patient-slider').append(singleItem);
-
-
-    })
-
-
+    $("#patient-slider").append(singleItem);
+  });
 }
 
 //Function to add patient images to the patients grid
 //===================================================
-function dynamicPatientsImages(){
+function dynamicPatientsImages() {
+  // Getting all doctors info
+  let patient = PATIENTS;
 
-    // Getting all doctors info
-    let patient = PATIENTS;
-
-    // Adding images to the with a wrapper
-    // and necessary data for isotope filter
-    patient.map((m, i) => {
-        let gridItem = `<div  class='grid-item ${ i == 0 ? "active" : ""}' ><div class="image-bg"><img data-id="${i}" src="${m.imagePath}"/></div></div>`;
-        $('#patients-grid').append(gridItem);
-    })
+  // Adding images to the with a wrapper
+  // and necessary data for isotope filter
+  patient.map((m, i) => {
+    let gridItem = `<div  class='grid-item ${
+      i == 0 ? "active" : ""
+    }' ><div class="image-bg"><img data-id="${i}" src="${
+      m.imagePath
+    }"/></div></div>`;
+    $("#patients-grid").append(gridItem);
+  });
 }
 
 // Function to init patients isotope
 //=====================================
 
-function patientsIsotope(){
-
-    $('#patients-grid').isotope({
-        itemSelector: '.grid-item',
-        layoutMode : 'masonry',
-        percentPosition: true,
-      })
-
+function patientsIsotope() {
+  $("#patients-grid").isotope({
+    itemSelector: ".grid-item",
+    layoutMode: "masonry",
+    percentPosition: true,
+  });
 }
 
-function patientInfoSlick(){
+function patientInfoSlick() {
+  $("#patient-slider").on("afterChange", function (e, s, d) {
+    $("#patients-grid .grid-item").removeClass("active");
+    let currentSlide = $("#patients-grid .grid-item")[s.currentSlide];
 
+    $(currentSlide).addClass("active");
+  });
 
-    $('#patient-slider').on('afterChange', function(e, s, d){
+  $("#patient-slider").slick({
+    autoplay: true,
+    autoplaySpeed: 4000,
+    arrows: false,
+    dots: true,
+    pauseOnHover: false,
+  });
 
-        $('#patients-grid .grid-item').removeClass('active');
-        let currentSlide = $('#patients-grid .grid-item')[s.currentSlide];
+  $(".slick-dots").find("button").remove();
 
-        $(currentSlide).addClass('active');
+  $("#patients-grid img").on("click", function () {
+    $("#patients-grid .grid-item").removeClass("active");
+    $(this).parents(".grid-item").addClass("active");
 
-    })
-
-    $('#patient-slider').slick({
-        autoplay : true,
-        autoplaySpeed : 4000,
-        arrows : false,
-        dots : true,
-        pauseOnHover : false
-    });
-
-    $('.slick-dots').find('button').remove();
-
-    $('#patients-grid img').on('click', function(){
-
-        $('#patients-grid .grid-item').removeClass('active');
-        $(this).parents('.grid-item').addClass('active');
-
-        let slide = $(this).data().id;
-        $('#patient-slider').slick('slickGoTo', slide);
-    })
-
-
-
+    let slide = $(this).data().id;
+    $("#patient-slider").slick("slickGoTo", slide);
+  });
 }
--- index.js
+++ index.js
@@ -1,29 +1,17 @@
-$(function(){
+$(function () {
+  init();
+  dynamicDoctorButtons();
+  dynamicDoctorImages();
+  doctorsIsotope();
+  doctorInfoChange();
+  setPatientData();
+  dynamicPatientsImages();
+  patientsIsotope();
+  patientInfoSlick();
+  changeDoctorSelection();
 
-
-    init();
-    dynamicDoctorButtons();
-    dynamicDoctorImages();
-    doctorsIsotope();
-    doctorInfoChange();
-    setPatientData();
-    dynamicPatientsImages();
-    patientsIsotope();
-    patientInfoSlick();
-
-
-    $('#sticky-header').sticky({
-        topSpacing : 0,
-        zIndex : 1
-    });
-
-    
-    
-
-    
-    
-  
-
-
-    
-})
+  $("#sticky-header").sticky({
+    topSpacing: 0,
+    zIndex: 1,
+  });
+});
