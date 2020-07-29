f4a1dad6c6c26240b516798f0620ec260d420ed6
itsmechetan <chtngupt@gmail.com>
2020-07-29 21:57:48 +0530

    Doctor's, Patients & Video data update

--- PATIENTS.js
+++ PATIENTS.js
@@ -26,19 +26,19 @@ let PATIENTS_INFO =
 
         "id"                        : "P2",
         "imagePath"                 : "assets/Patients/gnanakumar.png",
-        "name"                      : "Gnanakumar",
+        "name"                      : "Gnana kumar",
         "year_of_desease"           : "8",
         "reversed_days"             : "68",
-        "disease_type"              : "Blood Presure",
-        "point_before"              : "7.6",
-        "point_after"               : "3.9",
+        "disease_type"              : "Blood Sugar",
+        "point_before"              : "250",
+        "point_after"               : "140",
         "status_before"             : "Dangerous",
         "status_after"              : "Normalised",
         "medicine_count_before"     : "4",
         "medicine_count_after"      : "0",
         "medicines_before"          : "Insulin, Metformin, Glimepride, Dapaglif",
         "medicines_after"           : "stopped",
-        "comment"                   : "Why didn’t I know about this earlier – I have been looking for something like this."
+        "comment"                   : "i am aging in reverse since i started twin!  i strongly recommend this treatment to everyone looking for a change"
     },
 
     {
@@ -46,9 +46,9 @@ let PATIENTS_INFO =
         "id"                        : "P3",
         "imagePath"                 : "assets/Patients/kailasam.png",
         "name"                      : "Kailasam",
-        "year_of_desease"           : "8",
+        "year_of_desease"           : "14",
         "reversed_days"             : "68",
-        "disease_type"              : "Blood Presure",
+        "disease_type"              : "Blood Sugar",
         "point_before"              : "7.6",
         "point_after"               : "3.9",
         "status_before"             : "Dangerous",
@@ -57,7 +57,7 @@ let PATIENTS_INFO =
         "medicine_count_after"      : "0",
         "medicines_before"          : "Insulin, Metformin, Glimepride, Dapaglif",
         "medicines_after"           : "stopped",
-        "comment"                   : "Why didn’t I know about this earlier – I have been looking for something like this."
+        "comment"                   : "Twin is blessing to mankind! i hope everybody can benefit from this treatment"
     },
 
     {
@@ -65,18 +65,18 @@ let PATIENTS_INFO =
         "id"                        : "P4",
         "imagePath"                 : "assets/Patients/manzoor.png",
         "name"                      : "Manzoor",
-        "year_of_desease"           : "8",
-        "reversed_days"             : "68",
-        "disease_type"              : "Blood Presure",
-        "point_before"              : "7.6",
-        "point_after"               : "3.9",
+        "year_of_desease"           : "13",
+        "reversed_days"             : "30",
+        "disease_type"              : "Blood Sugar",
+        "point_before"              : "260",
+        "point_after"               : "109",
         "status_before"             : "Dangerous",
         "status_after"              : "Normalised",
         "medicine_count_before"     : "4",
         "medicine_count_after"      : "0",
         "medicines_before"          : "Insulin, Metformin, Glimepride, Dapaglif",
         "medicines_after"           : "stopped",
-        "comment"                   : "Why didn’t I know about this earlier – I have been looking for something like this."
+        "comment"                   : "I teared up with joy when my glucose levels dropped without medicines!"
     },
 
     {
@@ -85,17 +85,17 @@ let PATIENTS_INFO =
         "imagePath"                 : "assets/Patients/meera.png",
         "name"                      : "Meera",
         "year_of_desease"           : "8",
-        "reversed_days"             : "68",
-        "disease_type"              : "Blood Presure",
-        "point_before"              : "7.6",
-        "point_after"               : "3.9",
+        "reversed_days"             : "90",
+        "disease_type"              : "Blood Sugar",
+        "point_before"              : "250",
+        "point_after"               : "120",
         "status_before"             : "Dangerous",
         "status_after"              : "Normalised",
         "medicine_count_before"     : "4",
         "medicine_count_after"      : "0",
         "medicines_before"          : "Insulin, Metformin, Glimepride, Dapaglif",
         "medicines_after"           : "stopped",
-        "comment"                   : "Why didn’t I know about this earlier – I have been looking for something like this."
+        "comment"                   : "Twin has made a real difference in my life. i feel overjoyed and completely protected"
     },
 
     {
@@ -103,9 +103,9 @@ let PATIENTS_INFO =
         "id"                        : "P6",
         "imagePath"                 : "assets/Patients/ramanujam.png",
         "name"                      : "Ramanujam",
-        "year_of_desease"           : "8",
+        "year_of_desease"           : "5",
         "reversed_days"             : "68",
-        "disease_type"              : "Blood Presure",
+        "disease_type"              : "Blood Sugar",
         "point_before"              : "7.6",
         "point_after"               : "3.9",
         "status_before"             : "Dangerous",
@@ -114,7 +114,7 @@ let PATIENTS_INFO =
         "medicine_count_after"      : "0",
         "medicines_before"          : "Insulin, Metformin, Glimepride, Dapaglif",
         "medicines_after"           : "stopped",
-        "comment"                   : "Why didn’t I know about this earlier – I have been looking for something like this."
+        "comment"                   : "Twin is keeping me healthy and engaged, and I am enjoying every moment of the program."
     },
 
     {
@@ -122,18 +122,18 @@ let PATIENTS_INFO =
         "id"                        : "P7",
         "imagePath"                 : "assets/Patients/sambath.png",
         "name"                      : "Sambath",
-        "year_of_desease"           : "8",
-        "reversed_days"             : "68",
-        "disease_type"              : "Blood Presure",
-        "point_before"              : "7.6",
-        "point_after"               : "3.9",
+        "year_of_desease"           : "9",
+        "reversed_days"             : "50",
+        "disease_type"              : "Blood Sugar",
+        "point_before"              : "200",
+        "point_after"               : "104",
         "status_before"             : "Dangerous",
         "status_after"              : "Normalised",
         "medicine_count_before"     : "4",
         "medicine_count_after"      : "0",
         "medicines_before"          : "Insulin, Metformin, Glimepride, Dapaglif",
         "medicines_after"           : "stopped",
-        "comment"                   : "Why didn’t I know about this earlier – I have been looking for something like this."
+        "comment"                   : "Twins techonology and precision nutrition reversed my diabetes and stopped my medicines.this was the change i was looking for"
     },
     
     {
@@ -142,17 +142,17 @@ let PATIENTS_INFO =
         "imagePath"                 : "assets/Patients/saraswathi.png",
         "name"                      : "Saraswathi",
         "year_of_desease"           : "8",
-        "reversed_days"             : "68",
-        "disease_type"              : "Blood Presure",
-        "point_before"              : "7.6",
-        "point_after"               : "3.9",
+        "reversed_days"             : "90",
+        "disease_type"              : "Blood Sugar",
+        "point_before"              : "300",
+        "point_after"               : "130",
         "status_before"             : "Dangerous",
         "status_after"              : "Normalised",
         "medicine_count_before"     : "4",
         "medicine_count_after"      : "0",
         "medicines_before"          : "Insulin, Metformin, Glimepride, Dapaglif",
         "medicines_after"           : "stopped",
-        "comment"                   : "Why didn’t I know about this earlier – I have been looking for something like this."
+        "comment"                   : "With twin, all my diabetes symptoms are gone - without medicines. i feel like free bird"
     },
 
     {
@@ -160,9 +160,9 @@ let PATIENTS_INFO =
         "id"                        : "P9",
         "imagePath"                 : "assets/Patients/sharmila.png",
         "name"                      : "Sharmila",
-        "year_of_desease"           : "8",
+        "year_of_desease"           : "5",
         "reversed_days"             : "68",
-        "disease_type"              : "Blood Presure",
+        "disease_type"              : "Blood Sugar",
         "point_before"              : "7.6",
         "point_after"               : "3.9",
         "status_before"             : "Dangerous",
@@ -171,7 +171,7 @@ let PATIENTS_INFO =
         "medicine_count_after"      : "0",
         "medicines_before"          : "Insulin, Metformin, Glimepride, Dapaglif",
         "medicines_after"           : "stopped",
-        "comment"                   : "Why didn’t I know about this earlier – I have been looking for something like this."
+        "comment"                   : "Taking care of myself is easier with twin. my energy is great and i feel amazing"
     },
 
     {
@@ -179,9 +179,9 @@ let PATIENTS_INFO =
         "id"                        : "P10",
         "imagePath"                 : "assets/Patients/sreenivasan.png",
         "name"                      : "Sreenivasan",
-        "year_of_desease"           : "8",
-        "reversed_days"             : "68",
-        "disease_type"              : "Blood Presure",
+        "year_of_desease"           : "14",
+        "reversed_days"             : "60",
+        "disease_type"              : "Blood Sugar",
         "point_before"              : "7.6",
         "point_after"               : "3.9",
         "status_before"             : "Dangerous",
@@ -190,7 +190,7 @@ let PATIENTS_INFO =
         "medicine_count_after"      : "0",
         "medicines_before"          : "Insulin, Metformin, Glimepride, Dapaglif",
         "medicines_after"           : "stopped",
-        "comment"                   : "Why didn’t I know about this earlier – I have been looking for something like this."
+        "comment"                   : "Twin treatment saved me from a life constrained by medicines. it's a 5 star patient experience"
     },
 
     {
@@ -198,18 +198,18 @@ let PATIENTS_INFO =
         "id"                        : "P11",
         "imagePath"                 : "assets/Patients/yusuf.png",
         "name"                      : "Yusuf",
-        "year_of_desease"           : "8",
+        "year_of_desease"           : "3",
         "reversed_days"             : "68",
-        "disease_type"              : "Blood Presure",
-        "point_before"              : "7.6",
-        "point_after"               : "3.9",
+        "disease_type"              : "Blood Sugar",
+        "point_before"              : "430",
+        "point_after"               : "100",
         "status_before"             : "Dangerous",
         "status_after"              : "Normalised",
         "medicine_count_before"     : "4",
         "medicine_count_after"      : "0",
         "medicines_before"          : "Insulin, Metformin, Glimepride, Dapaglif",
         "medicines_after"           : "stopped",
-        "comment"                   : "Why didn’t I know about this earlier – I have been looking for something like this."
+        "comment"                   : "Twin treatment saved me from diabetes. i'm proud of the results completely due to personalized nutrition with zero medicines"
     },
 
 
